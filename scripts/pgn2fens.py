42d28a93994117f18ee7066650b8ceb7925aa4cc
330311 <330311@uwr.edu.pl>
2024-01-19 22:57:09 +0100

    Skrypt PGN -> lista FEN-ów

---
+++ scripts/pgn2fens.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Konwertuje plik z partią szachową w notacji PGN do listy FEN-ów (po każdym ruchu)
+# Wymagania: python-chess
+
+import os
+import sys
+import chess
+import chess.pgn
+
+with open(sys.argv[1],"r") as input_f,open(sys.argv[2],"w") as output_f:
+    game=chess.pgn.read_game(input_f)
+    board=chess.Board()
+
+    for move in game.mainline_moves():
+        board.push(move)
+        output_f.write(board.epd()+'\n')
+
+    
+
